16086693c9c35f2010e2ac9ac7f01325d56a39cc
Mohamed Ahmed <77528820+momo46060@users.noreply.github.com>
2025-07-02 11:49:26 +0300

    \

--- src/main/java/com/validationwiththymeleaf/model/Customer.java
+++ src/main/java/com/validationwiththymeleaf/model/Customer.java
@@ -1,7 +1,6 @@
 package com.validationwiththymeleaf.model;
 
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -10,6 +9,14 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min = 1, message = "is required")
     private  String lastName;
+    @NotNull(message = "is required")
+    @Min(value = 1, message = " too low")
+    @Max(value =10, message = " too high")
+    private  Integer passes;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "must be a 5 digit number")
+    private  String postalCode;
+
 
     public String getFirstName() {
         return firstName;
@@ -30,8 +37,26 @@ public class Customer {
     public Customer() {
     }
 
-    public Customer(String firstName, String lastName) {
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Customer(String firstName, String lastName, Integer passes, String postalCode) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.passes = passes;
+        this.postalCode = postalCode;
+    }
+
+    public Integer getPasses() {
+        return passes;
+    }
+
+    public void setPasses(Integer passes) {
+        this.passes = passes;
     }
 }
